c9597bffc65a44eba0a8e0840fb22505f065d155
Leon Zhao <lzhao92@gmail.com>
2021-10-21 21:25:40 -0400

    bug fix: reset game feature fixed and display winner text added

--- src/scenes/FgScene.js
+++ src/scenes/FgScene.js
@@ -44,22 +44,16 @@ export default class FgScene extends Scene {
 			game.config.height - 20
 		)
 
-		// Reset Button
-		const resetButton = this.add.image(500, 50, 'resetButton')
-			.setInteractive()
-			.on('pointerdown', () => this.scene.restart())
-
 		// Scoreboard
-		this.scoreboard1 = new Scoreboard(this, 250, 50, 'scoreboard', 0);
-		this.scoreboard2 = new Scoreboard(this, 750, 50, 'scoreboard', 0);
-		this.scoreboard2.flipX = true;
-		// this.add.image(250,50, 'scoreboard')
+		this.scoreboard1 = new Scoreboard(this, 250, 50, 'scoreboard', 0)
+		this.scoreboard2 = new Scoreboard(this, 750, 50, 'scoreboard', 0)
+		this.scoreboard2.flipX = true
 
 		// Player 1
-    this.slime = new Slime(this, 200, 550, 'slime', 0, {
-      shape: slimeShape.slime2,
-      frictionStatic: 0,
-    });
+		this.slime = new Slime(this, 200, 550, 'slime', 0, {
+			shape: slimeShape.slime2,
+			frictionStatic: 0,
+		})
 
 		// Player 2
 		this.slime2 = new Slime(this, 800, 550, 'slime', 0, {
@@ -92,26 +86,36 @@ export default class FgScene extends Scene {
 
 		// Event Emitter
 		const emitter = new Phaser.Events.EventEmitter()
-    emitter.on('reset', () => {
-      this.slime.resetSlime(200, 550);
-      this.slime2.resetSlime(800, 550);
-    })
-		emitter.on('player1Scores', () => this.score1++);
-		emitter.on('player2Scores', () => this.score2++);
+		emitter.on('reset', () => {
+			this.slime.resetSlime(200, 550)
+			this.slime2.resetSlime(800, 550)
+		})
+		emitter.on('player1Scores', () => this.score1++)
+		emitter.on('player2Scores', () => this.score2++)
 
 		// Collisions
-    const { bottom } = this.matter.world.walls;
+		const { bottom } = this.matter.world.walls
 		this.matterCollision.addOnCollideStart({
 			objectA: this.volleyball,
 			objectB: bottom,
 			callback: function (eventData) {
 				const { gameObjectA } = eventData
 				let pointGoesTo = gameObjectA.resetBall()
-        emitter.emit('reset')
-        if (pointGoesTo) emitter.emit('player1Scores');
-        else emitter.emit('player2Scores');
-      }
+				emitter.emit('reset')
+				if (pointGoesTo) emitter.emit('player1Scores')
+				else emitter.emit('player2Scores')
+			},
 		})
+
+		// Reset Button
+		const resetButton = this.add
+			.image(500, 50, 'resetButton')
+			.setInteractive()
+			.on('pointerdown', () => {
+				this.score1 = 0;
+				this.score2 = 0;
+				this.scene.restart();
+			})
 	}
 
 	update() {
@@ -120,13 +124,13 @@ export default class FgScene extends Scene {
 		this.scoreboard1.update(this.score1);
 		this.scoreboard2.update(this.score2);
 		if (this.score1 === 6 || this.score2 === 6) {
-			this.scene.stop();
+			let winner;
+			if (this.score1 === 6) winner = 1;
+			else winner = 2;
+			this.volleyball.destroy()
+			this.add.text(500, 150, `Player ${winner} wins!`, {fontSize: 24}).setOrigin(.5)
+
 		}
 	}
 
-	end () {
-		if (this.score1 === 6 || this.score2 === 6) {
-			this.scene.restart();
-		}
-	}
 }
